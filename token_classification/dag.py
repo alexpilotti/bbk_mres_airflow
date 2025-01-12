3a3b51a288faf3a43d4e187ab355c12fad83f3bb
Alessandro Pilotti <apilotti@cloudbasesolutions.com>
2025-01-12 21:38:47 +0200

    Add token_classification DAG

---
+++ token_classification/dag.py
@@ -0,0 +1,155 @@
+from datetime import datetime, timedelta
+
+from airflow.models.dag import DAG
+
+from airflow.models import Variable
+from airflow.operators.email import EmailOperator
+from airflow.providers.sftp.hooks.sftp import SFTPHook
+from airflow.providers.ssh.hooks.ssh import SSHHook
+from airflow.utils.task_group import TaskGroup
+
+from bbk_mres_airflow import k8s
+from bbk_mres_airflow.token_classification import tasks
+from bbk_mres_airflow import utils
+
+MODEL_BALM_PAIRED = "BALM-paired"
+MODEL_ANTIBERTY = "AntiBERTy"
+MODEL_ANTIBERTA2 = "AntiBERTa2"
+MODEL_ESM2_650M = "ESM2-650M"
+MODEL_ESM2_150M = "ESM2-150M"
+MODEL_ESM2_35M = "ESM2-35M"
+MODEL_ESM2_3B = "ESM2-3B"
+MODEL_ESM2_8M = "ESM2-8M"
+
+MODEL_NAME_FT_ESM2 = "ft-ESM2"
+
+VAR_CHAIN = "chain"
+CHAIN_H = "H"
+CHAIN_L = "L"
+CHAIN_HL = "HL"
+
+EXTERNAL_MODELS_PATH = f"{k8s.DATA_PATH}/pre_trained_models"
+BALM_MODEL_PATH = f"{
+    EXTERNAL_MODELS_PATH}/BALM-paired_LC-coherence_90-5-5-split_122222/"
+FT_ESM2_MODEL_PATH = f"{EXTERNAL_MODELS_PATH}/ESM2-650M_paired-fine-tuning/"
+
+OUTPUT_PATH = f"{k8s.DATA_PATH}/output"
+
+TOKEN_PREDICTION_RMD = f"token_prediction.Rmd"
+TOKEN_PREDICTION_OUTPUT_FILENAME = "token_prediction.html"
+
+
+with DAG(
+    "BBK-MRes-token-classification",
+    default_args={
+        "depends_on_past": False,
+        "email": ["apilot02@student.bbk.ac.uk"],
+        "email_on_failure": False,
+        "email_on_retry": False,
+        "retries": 2,
+        "retry_delay": timedelta(seconds=5),
+        # 'queue': 'bash_queue',
+        # 'pool': 'backfill',
+        # 'priority_weight': 10,
+        # 'end_date': datetime(2016, 1, 1),
+        # 'wait_for_downstream': False,
+        # 'sla': timedelta(hours=2),
+        # 'execution_timeout': timedelta(seconds=300),
+        # 'on_failure_callback': some_function, # or list of functions
+        # 'on_success_callback': some_other_function, # or list of functions
+        # 'on_retry_callback': another_function, # or list of functions
+        # 'sla_miss_callback': yet_another_function, # or list of functions
+        # 'on_skipped_callback': another_function, #or list of functions
+        # 'trigger_rule': 'all_success'
+    },
+    max_active_runs=1,
+    description="BBK-MRes Token Classification",
+    schedule_interval=None,
+    # schedule=timedelta(days=1),
+    start_date=datetime(2024, 8, 1),
+    catchup=False,
+    tags=["bbk"],
+) as dag:
+    chain = Variable.get(VAR_CHAIN, CHAIN_H)
+    if chain not in [CHAIN_H, CHAIN_L, CHAIN_HL]:
+        raise Exception(f"Invalid chain: {chain}")
+
+    task_info = [
+        (MODEL_ANTIBERTY, None, False, None, 1),
+        (MODEL_ANTIBERTA2, None, False, None, 1),
+        (MODEL_BALM_PAIRED, BALM_MODEL_PATH, False, None, 1),
+        (MODEL_ESM2_8M, None, False, None, 1),
+        (MODEL_ESM2_35M, None, False, None, 1),
+        (MODEL_ESM2_150M, None, False, None, 1),
+        (MODEL_ESM2_650M, None, False, None, 2),
+        (MODEL_ESM2_650M, FT_ESM2_MODEL_PATH, True, MODEL_NAME_FT_ESM2, 2),
+        (MODEL_ESM2_3B, None, False, None, 4)
+    ]
+
+    predict_tasks = []
+
+    for (model, model_path_pt, use_default_model_tokenizer,
+         task_model_name, num_gpus) in task_info:
+        if not task_model_name:
+            task_model_name = model
+
+        with TaskGroup(group_id=task_model_name) as tg:
+            with TaskGroup(group_id=f"training") as tg1:
+                task_check_train, task_train = tasks.create_fine_tuning_tasks(
+                    model, chain, model_path_pt, use_default_model_tokenizer,
+                    task_model_name, num_gpus)
+
+            with TaskGroup(group_id=f"predict") as tg1:
+                (task_check_predict_ft,
+                 task_predict_ft) = tasks.create_label_prediction_tasks(
+                    model, chain, model_path_pt, use_default_model_tokenizer,
+                    task_model_name, pre_trained=False)
+
+                (task_check_predict_pt,
+                 task_predict_pt) = tasks.create_label_prediction_tasks(
+                    model, chain, model_path_pt, use_default_model_tokenizer,
+                    task_model_name, pre_trained=True)
+
+                task_train >> task_check_predict_ft
+
+                predict_tasks.extend([task_predict_ft, task_predict_pt])
+
+    with TaskGroup(group_id=f"reports") as tg:
+        token_prediction_rmd = tasks.create_rmarkdown_task(
+            "token_prediction_rmd",
+            TOKEN_PREDICTION_RMD,
+            OUTPUT_PATH,
+            TOKEN_PREDICTION_OUTPUT_FILENAME,
+            chain)
+
+        predict_tasks >> token_prediction_rmd
+
+        data_url = f"{utils.get_base_url()}/data/"
+
+        send_success_email = EmailOperator(
+            task_id="send_success_email",
+            to="{{ var.value.email_to }}",
+            subject="BBK-MRes token classification tasks completed",
+            html_content=(
+                '<p>'
+                'Start time: {{ data_interval_start }}<br/>'
+                '</p>'
+                '<h3>Results</h3>'
+                '<ul>'
+                '  <li>'
+                '    <a href="{{ params.data_url }}/output/{{ run_id }}/'
+                'token_prediction.html">Token prediction</a>'
+                '  </li>'
+                '</ul>'
+                '<br/>'
+                '<a href="{{ params.get_dag_run_url(dag.dag_id, run_id) }}">'
+                'Mode details</a>'
+                '<br/>'
+                '<a href="{{ params.data_url }}">All data</a>'
+                '</p>'),
+            params={
+                "data_url": data_url,
+                "get_dag_run_url": utils.get_dag_run_url}
+            )
+
+        send_success_email << token_prediction_rmd
---
+++ token_classification/tasks.py
@@ -0,0 +1,154 @@
+from bbk_mres_airflow import fs_compare_operators
+from bbk_mres_airflow import k8s
+
+PRE_TRAINED = "PT"
+FINE_TUNED = "FT"
+
+MODELS_PATH = f"{k8s.DATA_PATH}/models"
+
+FINE_TUNING_INPUT_PATH = f"{k8s.DATA_PATH}/tokens_data.parquet"
+FINE_TUNING_OUTPUT_PATH = f"{MODELS_PATH}/" + "{model}_{chain}/"
+FINE_TUNING_OUTPUT_PATH_CHECK = FINE_TUNING_OUTPUT_PATH + "config.json"
+
+PREDICT_METRICS_PATH = (
+    f"{k8s.DATA_PATH}/" +
+    "token_predict_metrics_{model}_{chain}_{pre_trained}.json")
+PREDICT_LABELS_PATH = (
+    f"{k8s.DATA_PATH}/" +
+    "token_prediction_{model}_{chain}_{pre_trained}.parquet")
+
+PREDICT_GPUS = 1
+
+CUDA_CONTAINER_IMAGE = "registry.bbk-mres:5000/bbk-mres-cuda:latest"
+R_CONTAINER_IMAGE = "registry.bbk-mres:5000/bbk-mres-r:latest"
+
+FINE_TUNING_CMD = (
+    "git fetch && git checkout main && "
+    "python3 attention_comparison/cli.py "
+    "token-fine-tuning -m {{ params.model }} "
+    "-i {{ params.input }} -o {{ params.output }} "
+    "-c {{ params.chain }}"
+    "{% if params.model_path %} -p {{ params.model_path }}"
+    "{% endif %}{% if params.use_default_model_tokenizer %} "
+    "--use-default-model-tokenizer"
+    "{% endif %}")
+
+PREDICT_CMD = (
+    "git fetch && git checkout main && "
+    "python3 attention_comparison/cli.py "
+    "token-prediction -m {{ params.model }} "
+    "-i {{ params.input }} -o {{ params.output_metrics }} "
+    "-c {{ params.chain }} -P {{ params.output_labels}}"
+    "{% if params.model_path %} -p {{ params.model_path }}"
+    "{% endif %}{% if params.use_default_model_tokenizer %} "
+    "--use-default-model-tokenizer"
+    "{% endif %}")
+
+RMARKDOWN_CMD = (
+    "git fetch && git checkout main && "
+    "mkdir -p '{{ params.output_base_dir }}/{{ run_id }}' && "
+    "/usr/bin/Rscript -e "
+    "\"rmarkdown::render('{{ params.rmd_path }}', "
+    "output_file = '{{ params.output_base_dir }}/{{ run_id }}/"
+    "{{ params.output_filename }}', "
+    "params = list({{ params.params }}))\"")
+
+
+def create_fine_tuning_tasks(model, chain, model_path=None,
+                             use_default_model_tokenizer=None,
+                             task_model_name=None, num_gpus=1):
+
+    input_path = FINE_TUNING_INPUT_PATH
+    output_path_check = FINE_TUNING_OUTPUT_PATH_CHECK.format(
+        model=task_model_name, chain=chain)
+    output_path = FINE_TUNING_OUTPUT_PATH.format(
+        model=task_model_name, chain=chain)
+
+    task_check = fs_compare_operators.ComparePathDatetimesSensor(
+        task_id=f"check_update_model_{task_model_name}_{chain}",
+        path1=input_path,
+        path2=output_path_check,
+        timeout=0,
+        trigger_rule="none_failed"
+    )
+
+    task_train = k8s.create_pod_operator(
+        task_id=f"fine_tuning_{task_model_name}_{chain}",
+        image=CUDA_CONTAINER_IMAGE,
+        num_gpus=num_gpus,
+        command=FINE_TUNING_CMD,
+        params={"model": model,
+                "input": input_path,
+                "output": output_path,
+                "chain": chain,
+                "model_path": model_path,
+                "use_default_model_tokenizer": use_default_model_tokenizer}
+    )
+
+    task_check >> task_train
+
+    return task_check, task_train
+
+
+def create_label_prediction_tasks(model, chain, model_path=None,
+                                  use_default_model_tokenizer=None,
+                                  task_model_name=None, pre_trained=True):
+
+    pre_trained_str = (PRE_TRAINED if pre_trained else FINE_TUNED)
+
+    input_path = FINE_TUNING_INPUT_PATH
+    output_metrics_path = PREDICT_METRICS_PATH.format(
+        model=task_model_name, chain=chain, pre_trained=pre_trained_str)
+    output_labels_path = PREDICT_LABELS_PATH.format(
+        model=task_model_name, chain=chain, pre_trained=pre_trained_str)
+
+    check_input_paths = [input_path]
+    if not pre_trained:
+        model_path_check = FINE_TUNING_OUTPUT_PATH_CHECK.format(
+            model=task_model_name, chain=chain)
+        check_input_paths.append(model_path_check)
+
+    task_check = fs_compare_operators.ComparePathDatetimesSensor(
+        task_id=(f"check_update_predict_{task_model_name}_{chain}"
+                 f"_{pre_trained_str}"),
+        path1=check_input_paths,
+        path2=output_metrics_path,
+        timeout=0,
+        trigger_rule="none_failed"
+    )
+
+    if not pre_trained:
+        model_path = FINE_TUNING_OUTPUT_PATH.format(
+            model=task_model_name, chain=chain)
+
+    task_predict = k8s.create_pod_operator(
+        task_id=f"predict_{task_model_name}_{chain}_{pre_trained_str}",
+        image=CUDA_CONTAINER_IMAGE,
+        num_gpus=PREDICT_GPUS,
+        command=PREDICT_CMD,
+        params={"model": model,
+                "input": input_path,
+                "output_metrics": output_metrics_path,
+                "output_labels": output_labels_path,
+                "chain": chain,
+                "model_path": model_path,
+                "use_default_model_tokenizer": use_default_model_tokenizer}
+    )
+
+    task_check >> task_predict
+
+    return task_check, task_predict
+
+
+def create_rmarkdown_task(task_id, rmd_path, output_base_dir,
+                          output_filename, chain):
+    return k8s.create_pod_operator(
+        task_id=task_id,
+        image=R_CONTAINER_IMAGE,
+        command=RMARKDOWN_CMD,
+        params={"rmd_path": rmd_path,
+                "output_base_dir": output_base_dir,
+                "output_filename": output_filename,
+                "params": f"chain='{chain}', data_path='{k8s.DATA_PATH}'"},
+        trigger_rule="none_failed"
+    )
