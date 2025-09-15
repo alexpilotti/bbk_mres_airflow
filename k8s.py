e242690d227e85f736d38fb5ed1d592d6555c14e
Alessandro Pilotti <apilotti@cloudbasesolutions.com>
2025-09-15 22:35:00 +0300

    Add /dev/shm shared memory in K8s GPU pods

--- k8s.py
+++ k8s.py
@@ -7,6 +7,7 @@ from bbk_mres_airflow import common
 K8S_CONN = "k8s_conn"
 POOL = "k8s_gpu_pool"
 K8S_POD_STARTUP_TIMEOUT_SECONDS = 7200
+SHM_SIZE_LIMIT = "1Gi"
 
 
 def create_pod_operator(task_id, image, command, params, num_gpus=0,
@@ -17,11 +18,13 @@ def create_pod_operator(task_id, image, command, params, num_gpus=0,
             claim_name="bbk-mres-data-pvc"
         ),
     )
+    volumes = [data_volume]
 
     data_volume_mount = k8s.V1VolumeMount(
         name="data-volume",
         mount_path=common.DATA_PATH
     )
+    volume_mounts = [data_volume_mount]
 
     resources = None
     pod_template_dict = None
@@ -41,6 +44,20 @@ def create_pod_operator(task_id, image, command, params, num_gpus=0,
                 }
             }
 
+        # Add shared memory, recommended for DeepSpeed
+        shm_volume = k8s.V1Volume(
+            name="devshm",
+            empty_dir=k8s.V1EmptyDirVolumeSource(
+                medium="Memory", size_limit=SHM_SIZE_LIMIT)
+        )
+        volumes.append(shm_volume)
+
+        shm_volume_mount = k8s.V1VolumeMount(
+            name="devshm",
+            mount_path="/dev/shm"
+        )
+        volume_mounts.append(shm_volume_mount)
+
     return pod.KubernetesPodOperator(
         name=task_id,
         task_id=task_id,
@@ -48,8 +65,8 @@ def create_pod_operator(task_id, image, command, params, num_gpus=0,
         pod_template_dict=pod_template_dict,
         container_resources=resources,
         arguments=[command],
-        volumes=[data_volume],
-        volume_mounts=[data_volume_mount],
+        volumes=volumes,
+        volume_mounts=volume_mounts,
         is_delete_operator_pod=True,
         get_logs=True,
         log_events_on_failure=True,
